742afdc90d35ecebf0879ebb9191791f1555408c
camila breda <camilabbreda@gmail.com>
2025-09-02 13:25:09 -0300

    all languages config

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,35 @@
-"use client";
-import dynamic from "next/dynamic";
-import styles from "../styles/page.module.css";
-import { useState } from "react";
-import { Button } from "@mui/material";
-const BlipChatClient = dynamic(() => import("../components/blipchat"), {
+'use client'
+import dynamic from 'next/dynamic'
+import styles from '../styles/page.module.css'
+import { useState } from 'react'
+import { Button } from '@mui/material'
+const BlipChatClient = dynamic(() => import('../components/blipchat'), {
   ssr: false,
-});
+})
 
 export default function Home() {
-  const [selection, setSelection] = useState<string>("");
-  const [country, setCountry] = useState<string>("USA");
+  const [selection, setSelection] = useState<string>('')
+  const [country, setCountry] = useState<string>('USA')
   return (
     <div className={styles.page} id="corpo">
       <main className={styles.main}>
-        <div style={{ fontSize: "25px" }}>Pick a Country:</div>
+        <div style={{ fontSize: '25px' }}>Pick a Country:</div>
         <div
           style={{
-            width: "220px",
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "space between",
+            width: '560px',
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'space between',
           }}
         >
           <Button
             variant="contained"
             sx={{
-              backgroundColor: `${country === "USA" ? "grey" : "black"}`,
-              color: "white",
-              "&:hover": { backgroundColor: "#333" },
+              backgroundColor: `${country === 'USA' ? 'grey' : 'black'}`,
+              color: 'white',
+              '&:hover': { backgroundColor: '#333' },
             }}
-            onClick={() => setCountry("USA")}
+            onClick={() => setCountry('USA')}
           >
             USA
           </Button>
@@ -37,42 +37,78 @@ export default function Home() {
             variant="contained"
             sx={{
               backgroundColor: `${
-                country === "CANADA | EN" ? "grey" : "black"
+                country === 'CANADA | EN' ? 'grey' : 'black'
               }`,
-              color: "white",
-              "&:hover": { backgroundColor: "#333" },
+              color: 'white',
+              '&:hover': { backgroundColor: '#333' },
             }}
-            onClick={() => setCountry("CANADA | EN")}
+            onClick={() => setCountry('CANADA | EN')}
           >
             CANADA | EN
           </Button>
+          <Button
+            variant="contained"
+            disabled={true}
+            sx={{
+              backgroundColor: `${
+                country === 'CANADA | FR' ? 'grey' : 'black'
+              }`,
+              color: 'white',
+              '&:hover': { backgroundColor: '#333' },
+            }}
+            onClick={() => setCountry('CANADA | FR')}
+          >
+            CANADA | FR
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: `${country === 'BRAZIL' ? 'grey' : 'black'}`,
+              color: 'white',
+              '&:hover': { backgroundColor: '#333' },
+            }}
+            onClick={() => setCountry('BRAZIL')}
+          >
+            BRAZIL
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: `${country === 'MEXICO' ? 'grey' : 'black'}`,
+              color: 'white',
+              '&:hover': { backgroundColor: '#333' },
+            }}
+            onClick={() => setCountry('MEXICO')}
+          >
+            MEXICO
+          </Button>
         </div>
         <div>
-          <p style={{ fontSize: "25px" }}>Select a Chatbot to Chat With:</p>
+          <p style={{ fontSize: '25px' }}>Select a Chatbot to Chat With:</p>
         </div>
         <div
           style={{
-            display: "flex",
-            flexDirection: "row",
-            gap: "32px",
-            justifyContent: "center",
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '32px',
+            justifyContent: 'center',
             // backgroundColor: "red",
-            width: "600px",
+            width: '600px',
           }}
         >
           <div
-            style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+            style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}
           >
             <Button
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "ISO 9001" ? "grey" : "black"
+                  selection === 'ISO 9001' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("ISO 9001")}
+              onClick={() => setSelection('ISO 9001')}
             >
               ISO 9001
             </Button>
@@ -81,12 +117,12 @@ export default function Home() {
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "Digital Trust" ? "grey" : "black"
+                  selection === 'Digital Trust' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("Digital Trust")}
+              onClick={() => setSelection('Digital Trust')}
             >
               Digital Trust
             </Button>
@@ -94,29 +130,29 @@ export default function Home() {
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "Sustainability" ? "grey" : "black"
+                  selection === 'Sustainability' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("Sustainability")}
+              onClick={() => setSelection('Sustainability')}
             >
               Sustainability
             </Button>
           </div>
           <div
-            style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+            style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}
           >
             <Button
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "Health and Safety" ? "grey" : "black"
+                  selection === 'Health and Safety' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("Health and Safety")}
+              onClick={() => setSelection('Health and Safety')}
             >
               Health and Safety
             </Button>
@@ -124,12 +160,12 @@ export default function Home() {
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "System Management" ? "grey" : "black"
+                  selection === 'System Management' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("System Management")}
+              onClick={() => setSelection('System Management')}
             >
               System Management
             </Button>
@@ -137,29 +173,29 @@ export default function Home() {
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "Aerospace" ? "grey" : "black"
+                  selection === 'Aerospace' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("Aerospace")}
+              onClick={() => setSelection('Aerospace')}
             >
               Aerospace
             </Button>
           </div>
           <div
-            style={{ display: "flex", flexDirection: "column", gap: "16px" }}
+            style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}
           >
             <Button
               variant="contained"
               sx={{
                 backgroundColor: `${
-                  selection === "Automotive" ? "grey" : "black"
+                  selection === 'Automotive' ? 'grey' : 'black'
                 }`,
-                color: "white",
-                "&:hover": { backgroundColor: "#333" },
+                color: 'white',
+                '&:hover': { backgroundColor: '#333' },
               }}
-              onClick={() => setSelection("Automotive")}
+              onClick={() => setSelection('Automotive')}
             >
               Automotive
             </Button>
@@ -170,7 +206,7 @@ export default function Home() {
           <>
             <div
               style={{
-                fontSize: "25px",
+                fontSize: '25px',
               }}
             >
               <p>Click the chat icon to start the conversation... 👉</p>
@@ -180,12 +216,12 @@ export default function Home() {
         ) : (
           <div
             style={{
-              height: "30px",
+              height: '30px',
             }}
           ></div>
         )}
       </main>
       <footer className={styles.footer}></footer>
     </div>
-  );
+  )
 }
--- components/blipchat.tsx
+++ components/blipchat.tsx
@@ -1,78 +1,87 @@
-"use client";
-import { BlipChat } from "blip-chat-widget";
-import { useEffect, useState } from "react";
+'use client'
+import { BlipChat } from 'blip-chat-widget'
+import { useEffect, useState } from 'react'
 type tProps = {
-  selection: string;
-  country: string;
-};
+  selection: string
+  country: string
+}
 
 export default function Home({ selection, country }: tProps) {
-  const [option, setOption] = useState<string | undefined>(undefined);
-  const [appKey, setAppKey] = useState<string>("");
+  const [option, setOption] = useState<string | undefined>(undefined)
+  const [appKey, setAppKey] = useState<string>('')
 
-  const authType = `${process.env.NEXT_PUBLIC_BLIP_AUTH_TYPE}`;
-  const userIdentity = new Date().getTime().toString();
-  const userPassword = new Date().getTime().toString();
+  const authType = `${process.env.NEXT_PUBLIC_BLIP_AUTH_TYPE}`
+  const userIdentity = new Date().getTime().toString()
+  const userPassword = new Date().getTime().toString()
   useEffect(() => {
     switch (selection) {
-      case "ISO 9001":
-        setOption("ISO 9001");
-        break;
-      case "Digital Trust":
-        setOption("Digital Trust");
-        break;
-      case "Sustainability":
-        setOption("Sustainability");
-        break;
-      case "Health and Safety":
-        setOption("Health and Safety");
-        break;
-      case "System Management":
-        setOption("System Management");
-        break;
-      case "Aerospace":
-        setOption("Aerospace");
-        break;
-      case "Automotive":
-        setOption("Automotive");
-        break;
+      case 'ISO 9001':
+        setOption('ISO 9001')
+        break
+      case 'Digital Trust':
+        setOption('Digital Trust')
+        break
+      case 'Sustainability':
+        setOption('Sustainability')
+        break
+      case 'Health and Safety':
+        setOption('Health and Safety')
+        break
+      case 'System Management':
+        setOption('System Management')
+        break
+      case 'Aerospace':
+        setOption('Aerospace')
+        break
+      case 'Automotive':
+        setOption('Automotive')
+        break
       default:
-        setOption("ISO 9001");
-        break;
+        setOption('ISO 9001')
+        break
     }
     switch (country) {
-      case "USA":
-        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_USA}`);
-        break;
-      case "CANADA | EN":
-        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_CAN_EN}`);
-        break;
+      case 'USA':
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_USA}`)
+        break
+      case 'CANADA | EN':
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_CAN_EN}`)
+        break
+      case 'CANADA | FR':
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_CAN_FR}`)
+        break
+      case 'BRAZIL':
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_BR}`)
+        break
+      case 'MEXICO':
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_MX}`)
+        break
       default:
-        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_USA}`);
-        break;
+        setAppKey(`${process.env.NEXT_PUBLIC_BLIP_APP_KEY_USA}`)
+        break
     }
-  }, [selection, country]);
+  }, [selection, country])
 
   useEffect(() => {
-    if (!option || !appKey) return;
+    if (!option || !appKey) return
 
-    const existingChat = document.getElementById("blip-chat-container");
+    const existingChat = document.getElementById('blip-chat-container')
     if (existingChat) {
-      existingChat.remove();
+      existingChat.remove()
     }
 
-    const container = document.createElement("div");
-    container.id = "blip-chat-container";
-    document.body.appendChild(container);
+    const container = document.createElement('div')
+    container.id = 'blip-chat-container'
+    document.body.appendChild(container)
 
-    const script = document.createElement("script");
-    script.src = "https://unpkg.com/blip-chat-widget";
-    script.async = true;
+    const script = document.createElement('script')
+    script.src = 'https://unpkg.com/blip-chat-widget'
+    script.async = true
     script.onload = () => {
-      const blipClient = new BlipChat();
+      const blipClient = new BlipChat()
       blipClient
         .withAppKey(appKey)
-        .withButton({ color: "#2A2A2A", icon: "" })
+        .withButton({ color: '#2A2A2A', icon: '' })
         .withAuth({
           authType,
           userIdentity,
@@ -95,25 +104,25 @@ export default function Home({ selection, country }: tProps) {
             }
           `
         )
-        .withCustomCommonUrl("https://bsigroup.chat.blip.ai/")
+        .withCustomCommonUrl('https://bsi.chat.blip.ai/')
         .withEventHandler(BlipChat.LOAD_EVENT, function () {
           blipClient.sendMessage({
-            type: "text/plain",
-            content: "Start",
+            type: 'text/plain',
+            content: 'Start',
             metadata: {
-              "#blip.hiddenMessage": true,
+              '#blip.hiddenMessage': true,
             },
-          });
+          })
         })
-        .build();
-    };
-    document.body.appendChild(script);
+        .build()
+    }
+    document.body.appendChild(script)
 
     return () => {
-      script.remove();
-      container.remove();
-    };
-  }, [option, appKey]);
+      script.remove()
+      container.remove()
+    }
+  }, [option, appKey])
 
-  return null;
+  return null
 }
